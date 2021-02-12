f0b44030a8d1692c93c1dd0c66dee2bffe250fec
Themancodes <menocorde04@gmail.com>
2021-02-12 17:19:44 -0600

    So far

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,13 @@
 class Link < ApplicationRecord
+  validates :url, presence: true
+  validate :validate_url
+
+    def share_id
+    end
+
+    def share_url(base_url)
+        uri = URI.parse(base_url)
+        "#{uri.scheme}://#{uri.host}/#{share_id}"
+    end
+
 end
