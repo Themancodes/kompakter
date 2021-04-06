5719c1ef5594226df9c31cbb0238e38e8069b879
Themancodes <menocorde04@gmail.com>
2021-04-05 23:30:45 -0500

    Fix: Url Routes rework

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :links, except: :show
+  # resources :links, except: :show
+  resources :links
 
   #Parse the real shortened URL
-  Rails.application.routes.draw do
-    get '/s/:ending', to: 'links#show', as: :short
-  end
+  get '/s/:ending', to: 'links#redirect_to_link', as: :redirex
 
   root to: "links#index"
 
